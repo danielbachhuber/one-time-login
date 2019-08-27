21d649696ae61961ab3a6172a349c484e31d9346
Daniel Bachhuber <daniel@handbuilt.co>
2019-08-27 11:39:49 -0700

    Update NPM to avoid security issues

--- Gruntfile.js
+++ Gruntfile.js
@@ -7,17 +7,6 @@ module.exports = function( grunt ) {
 
 		pkg: grunt.file.readJSON( 'package.json' ),
 
-		addtextdomain: {
-			options: {
-				textdomain: 'one-time-login',
-			},
-			target: {
-				files: {
-					src: [ '*.php', '**/*.php', '!node_modules/**', '!php-tests/**', '!bin/**' ]
-				}
-			}
-		},
-
 		wp_readme_to_markdown: {
 			your_target: {
 				files: {
@@ -26,26 +15,9 @@ module.exports = function( grunt ) {
 			},
 		},
 
-		makepot: {
-			target: {
-				options: {
-					domainPath: '/languages',
-					mainFile: 'one-time-login.php',
-					potFilename: 'one-time-login.pot',
-					potHeaders: {
-						poedit: true,
-						'x-poedit-keywordslist': true
-					},
-					type: 'wp-plugin',
-					updateTimestamp: true
-				}
-			}
-		},
 	} );
 
-	grunt.loadNpmTasks( 'grunt-wp-i18n' );
 	grunt.loadNpmTasks( 'grunt-wp-readme-to-markdown' );
-	grunt.registerTask( 'i18n', ['addtextdomain', 'makepot'] );
 	grunt.registerTask( 'readme', ['wp_readme_to_markdown'] );
 
 	grunt.util.linefeed = '\n';
