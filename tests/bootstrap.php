3ac68a51f0e6cd65625a466cdb39bb45582ede41
Daniel Bachhuber <daniel@bachhuber.co>
2023-11-27 12:55:04 -0800

    Update WPCS to v3 (#44)
    * Update WPCS to v3
    
    * Fix PHPCS issues

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -17,7 +17,7 @@ require_once $_tests_dir . '/includes/functions.php';
  * Manually load the plugin being tested.
  */
 function _manually_load_plugin() {
-	require dirname( dirname( __FILE__ ) ) . '/one-time-login.php';
+	require dirname( __DIR__, 1 ) . '/one-time-login.php';
 }
 tests_add_filter( 'muplugins_loaded', '_manually_load_plugin' );
 
