b040f56b017230ef8ca9c4c869f881d0c247f0dc
Daniel Bachhuber <daniel.bachhuber@automattic.com>
2023-11-27 12:53:35 -0800

    Fix PHPCS issues

--- tests/test-one-time-login.php
+++ tests/test-one-time-login.php
@@ -89,7 +89,7 @@ class OneTimeLoginTest extends WP_UnitTestCase {
 	 * @param int     $count
 	 * @param array   $generated_count
 	 */
-	function test_generate_token( $delay_delete, $count, $generated_count ) {
+	public function test_generate_token( $delay_delete, $count, $generated_count ) {
 		$this->assertSame(
 			count( one_time_login_generate_tokens( self::$users['administrator'], $count, $delay_delete ) ),
 			$generated_count
